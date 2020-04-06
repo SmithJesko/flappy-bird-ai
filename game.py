13bf4b4d32efda84570f5de85012874194dfa7f1
SmithJesko <smithjesko@gmail.com>
2020-04-05 19:31:11 -0500

    added pipe class

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ import os
 import random
 
 
+
 window_width = 500
 window_height = 800
 
@@ -85,10 +86,57 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.image)
 
+
+class Pipe:
+    gap = 200
+    velocity = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.height = 0
+
+        self.top = 0
+        self.bottom = 0
+        self.pipe_top = pygame.transform.flip(pipe_image, False, True)
+        self.pipe_bottom = pipe_image
+
+        self.passed = False
+        self.set_height()
+
+    def set_height(self):
+        self.height = random.randrange(40, 450)
+        self.top = self.height - self.pipe_top.get_height()
+        self.bottom = self.height + self.gap
+
+    def move(self):
+        self.x -= self.velocity
+
+    def draw(self, window):
+        window.blit(self.pipe_top, (self.x, self.top))
+        window.blit(self.pipe_bottom, (self.x, self.bottom))
+
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_mask = pygame.mask.from_surface(self.pipe_top)
+        bottom_mask = pygame.mask.from_surface(self.pipe_bottom)
+
+        top_offset = (self.x - bird.x, self.top - round(bird.y))
+        bottom_offset = (self.x - bird.x, self.bottom - round(bird.y))
+
+        bottom_point = bird_mask.overlap(bottom_mask, bottom_offset)
+        top_point = bird_mask.overlap(top_mask, top_offset)
+
+        if top_point or bottom_point:
+            return True
+
+        return False
+
+
 def draw_window(window, bird):
     window.blit(background_image, (0, 0))
     bird.draw(window)
     pygame.display.update()
+
 
 def main():
     bird = Bird(200, 200)
